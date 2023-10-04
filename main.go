d32e79bdefba9d51c628be7aec7e810225ffe657
AkashKanteti <akashkanteti@gmail.com>
2023-10-04 13:24:49 +0530

    scanner

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Printf(quote.Go())
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// Write the data to standard input
+	scanner.Scan()
+	text := scanner.Text()
+	fmt.Println(text)
+	// scanner.Scan()
 
 }
+
+//echo 33 | ./main current usage
